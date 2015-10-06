5d185242446f5dc745c96665e1658234cec5f479
Michael Barbey <michael@mb-webdev.ch>
2015-10-06 09:06:56 +0200

    Remove private license comment

--- Listeners/HTTPResponseListener.php
+++ Listeners/HTTPResponseListener.php
@@ -1,15 +1,9 @@
 <?php
-/*
- * This file is part of the MBOptimization package.
- *
- * (c) Michael Barbey <michael@mb-webdev.ch>
- *
- * Do not distribute this file or any part of this package without the official authorizazion of mb-webdev
- */
 
 namespace MB\Bundle\OptimizationBundle\Listeners;
 
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
 class HTTPResponseListener
 {
     private $policy;
