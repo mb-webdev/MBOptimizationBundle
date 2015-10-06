b78d28c055961cbf3f865f03104e50566ec144d5
Michael Barbey <michael@mb-webdev.ch>
2015-10-06 09:06:30 +0200

    Remove private license comment

--- MBOptimizationBundle.php
+++ MBOptimizationBundle.php
@@ -1,11 +1,4 @@
 <?php
-/*
- * This file is part of the MBOptimization package.
- *
- * (c) Michael Barbey <michael@mb-webdev.ch>
- *
- * Do not distribute this file or any part of this package without the official authorizazion of mb-webdev
- */
 
 namespace MB\Bundle\OptimizationBundle;
 
